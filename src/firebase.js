e9ded719ad5ef3a545050b177fd5e79bb3eac733
fedor-resh <fedor_20052005@mail.ru>
2023-10-05 16:56:18 +0300

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAE1vdN42CkLjfABWwHkyMPryUKJUH_vbE",
+    authDomain: "freelance-32793.firebaseapp.com",
+    projectId: "freelance-32793",
+    storageBucket: "freelance-32793.appspot.com",
+    messagingSenderId: "1026880076216",
+    appId: "1:1026880076216:web:eae6c9103a662d41dd374d",
+    measurementId: "G-6DFS5GC0HN"
+};
+
+const app = initializeApp(firebaseConfig);
